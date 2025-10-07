c652986b89a84914947c17e74058e4ababf90ea9
Sathwik127 <alvalasathwik2023@gmail.com>
2025-10-07 02:08:42 -0400

    third

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
-
 from setuptools import find_packages,setup
 from typing import List
-
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
@@ -16,7 +14,6 @@ def get_requirements(file_path:str)->List[str]:
             requirements.remove(HYPEN_E_DOT)
     
     return requirements
-
 setup(
 name='mlproject',
 version='0.0.1',
