916d7e94385c955e4018637a9a54c83962104318
Sathwik127 <alvalasathwik2023@gmail.com>
2025-10-13 20:48:18 -0400

    fifth

--- src/Components/DataIngestion.py
+++ src/Components/DataIngestion.py
@@ -1,6 +1,11 @@
 import os
 import sys
 from src.Execptionhandling import CustomException
+from src.Components.DataTransform import DataTransformation
+from src.Components.DataTransform import DataTransformationConfig
+from src.Components.ModelTrainer import ModelTrainerConfig
+from src.Components.ModelTrainer import ModelTrainer
+
 import pandas as pd
 from src.logger import logging
 from sklearn.model_selection import train_test_split
@@ -45,4 +50,10 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
