690e3f0a5e683222be508b170711ec246d285fbc
Sathwik127 <alvalasathwik2023@gmail.com>
2025-11-05 02:00:05 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
+
 from sklearn.preprocessing import StandardScaler
 from src.Pipeline.PredictPipeline import CustomData, PredictPipeline
 import sys
